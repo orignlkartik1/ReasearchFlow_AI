ca482dd75c15579aa8977bae99bdf5e1152e05e5
orignlkartik1 <kart964364@gmail.com>
2026-09-04 02:07:26 +0530

    Making the workflow better

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -7,18 +7,17 @@ from .sub_agents.academic_webresearch.agent import academic_websearch_agent
 
 
 root_agent = Agent(
-        name="academic_coordinator",
-        model='gemini-2.5-flash',
-        description=(
-            "Analyzes seminal papers provided by users, provides research advice, "
-            "locates current papers relevant to the seminal paper, generates suggestions "
-            "for new research directions, and accesses web resources to acquire knowledge."
-        ),
-        instruction=prompt.ACADEMIC_COORDINATOR_PROMPT,
-        output_key="seminal_paper",
-        tools=[
-            AgentTool(agent=academic_newresearch_agent),
-            AgentTool(agent=academic_websearch_agent),
-        ],
-    )
-
+    name="academic_coordinator",
+    model='gemini-2.5-flash',
+    description=(
+        "Analyzes seminal papers provided by users, provides research advice, "
+        "locates current papers relevant to the seminal paper, generates suggestions "
+        "for new research directions, and accesses web resources to acquire knowledge."
+    ),
+    instruction=prompt.ACADEMIC_COORDINATOR_PROMPT,
+    output_key="seminal_paper",
+    tools=[
+        AgentTool(agent=academic_newresearch_agent),
+        AgentTool(agent=academic_websearch_agent),
+    ],
+)
